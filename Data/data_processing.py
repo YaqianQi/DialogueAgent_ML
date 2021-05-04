75f11a707a82d135ca4eda27571a25e9a2f83611
Joyce Liu <joyce@Joyces-MBP.fios-router.home>
2021-05-03 20:26:30 -0400

    add data processing folder

---
+++ Data/data_processing.py
@@ -0,0 +1,23 @@
+"""
+Convert data to input file that RASA can understand
+"""
+
+import json
+import nltk
+
+"""
+todo: add functions
+"""
+def generate_stories():
+    pass
+def generate_utterance():
+    pass
+def generate_intent():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
